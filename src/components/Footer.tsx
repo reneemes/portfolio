b01ee79cd5f49cbeb5538b1d23d77de7fd278f89
Renee Messersmith <morganrmessersmith@gmail.com>
2024-12-02 10:41:24 -0500

    fix: updated footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,8 +7,8 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/reneemes" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/reneemessersmith/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       {/* <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p> */}
       <p>Renee Messersmith</p>
