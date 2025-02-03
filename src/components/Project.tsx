96774bf05183a2523fc08e3312fd603ba196426c
Renee Messersmith <morganrmessersmith@gmail.com>
2025-02-03 13:04:21 -0500

    Feat: added Tracker to projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-// import mock01 from '../assets/images/mock01.png';
-// import mock02 from '../assets/images/mock02.png';
-// import mock03 from '../assets/images/mock03.png';
-// import mock04 from '../assets/images/mock04.png';
-// import mock05 from '../assets/images/mock05.png';
-// import mock06 from '../assets/images/mock06.png';
-// import mock07 from '../assets/images/mock07.png';
-// import mock08 from '../assets/images/mock08.png';
-// import mock09 from '../assets/images/mock09.png';
-// import mock10 from '../assets/images/mock10.png';
 import dtrh from '../assets/images/down-the-rabbit-hole.png';
 import rancid from '../assets/images/rancid.png';
+import tracker from '../assets/images/tracker.png'
 import '../assets/styles/Project.scss';
 
 import Chip from '@mui/material/Chip';
@@ -30,7 +21,6 @@ function Project() {
         "Webmock & VCR"
     ];
 
-
     const labelsSecond = [
         "React",
         "React Router",
@@ -40,10 +30,35 @@ function Project() {
         "Cypress"
     ];
 
+    const trackerTech = [
+        "React",
+        "React Router",
+        "Tailwind CSS",
+        "TypeScript",
+        "JavaScript",
+        "HTML",
+        "Cypress",
+        "Ruby on Rails",
+        "RSpec",
+        "SimpleCov",
+        "JWT Tokens"
+    ];
+
     return(
     <div className="projects-container" id="projects">
         <h1>Projects</h1>
         <div className="projects-grid">
+        <div className="project">
+                <a href="https://tracker-crm-fe-38f7cf1aaca5.herokuapp.com/" target="_blank" rel="noreferrer"><img src={tracker} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://tracker-crm-fe-38f7cf1aaca5.herokuapp.com/" target="_blank" rel="noreferrer"><h2>Tracker CRM</h2></a>
+                <p>An application for tracking companies, managing contacts, and organizing job applications all in one place.</p>
+                <div className="flex-chips">
+                    <span className="chip-title">Tech:</span>
+                    {trackerTech.map((label, index) => (
+                        <Chip key={index} className='chip' label={label} />
+                    ))}
+                </div>
+            </div>
             <div className="project">
                 <a href="https://down-the-rabbit-hole.netlify.app/" target="_blank" rel="noreferrer"><img src={dtrh} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://down-the-rabbit-hole.netlify.app/" target="_blank" rel="noreferrer"><h2>Down the Rabbit Hole</h2></a>
@@ -67,46 +82,6 @@ function Project() {
                     ))}
                 </div>
             </div>
-            {/* <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>Astro Raiders</h2></a>
-                <p>Developed and released a 2D shooting game with C# and Unity. This project is hosted on the Itch.io public marketplace.</p>
-            </div>
-            <div className="project">
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Datum: Integrated Learning Platform</h2></a>
-                <p>This is an online educational platform that provides high-quality, data science-focused learning resources in the Japanese language. I created the entire platform from scratch using Ruby on Rails.</p>
-            </div>
-            <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>WeManage: Real Estate Asset Management</h2></a>
-                <p>This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.</p>
-            </div>
-            <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>COVID-19 Case Management</h2></a>
-                <p>Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university's leadership in their decision-making processes.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Multiple Regression Property Analysis</h2></a>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
-            </div>
-            <div className="project">
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://holokai.byuh.edu/programs-of-study" target="_blank" rel="noreferrer"><h2>Programs of Study</h2></a>
-                <p>Designed and developed a custom component for a CMS-based platform (e.g., 'Brightspot') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.</p>
-            </div>
-            <div className="project">
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
-            </div>
-            <div className="project">
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
-                <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
-            </div> */}
         </div>
     </div>
     );
